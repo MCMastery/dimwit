f4ec01e63637514ded7508a8262d115aaa8bb1c7
MCMastery <craftyverse@gmail.com>
2016-09-22 13:20:08 -0400

    Size label now works

--- js/website.js
+++ js/website.js
@@ -1,6 +1,10 @@
 var index = window.location.href.indexOf("?");
-console.log(index);
 if (index >= 0) {
     code = window.location.href.substring(index + 1);
     document.getElementById("code").value = code;
+    updateSize();
+}
+
+function updateSize() {
+    document.getElementById("size").innerHTML = document.getElementById("code").value.length;
 }
